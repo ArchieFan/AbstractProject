6d4787a45f5b3ec583da6b1485d27a577136f73e
Archie Fan <archiefan@gmail.com>
2022-05-24 09:29:01 +0100

    Add project files.

---
+++ AbstractProject/AbsChild.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractProject
+{
+    internal class AbsChild : AbsParent 
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+    }
+}
---
+++ AbstractProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbsChild c = new AbsChild();
+            c.Add(150, 45);
+            c.Sub(100, 80);
+            c.Mul(5, 5);
+            c.Div(5, 5);
+
+            AbsParent p = c;
+            p.Add(150, 45);
+            p.Sub(100, 80);
+            p.Mul(5, 5);
+            p.Div(5, 5);
+            Console.ReadLine();
+        }
+    }
+}
